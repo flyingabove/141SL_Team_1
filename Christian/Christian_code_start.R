633a7f0acaaf5bebf203fa1fe6d64357534782a3
flyingabove <flyingabove@gmail.com>
2015-05-06 13:26:14 -0700

    nm

--- Christian/Christian_code_start.R
+++ Christian/Christian_code_start.R
@@ -1,5 +1,23 @@
 #install.packages("tuneR")
 
 library(tuneR)
-s10<-readWave("Baby_cry01.wav")
+s10<-readWave("test_pitch.wav")
 head(s10@left)
+
+
+runif(5)
+
+r_p_s<-function(num){
+  if(num<=.33){
+    return("R") 
+  }
+  else if (num>=.33&num<=.66){
+    return("P")
+  }
+  else {
+    return("S")
+  }
+}
+  
+  
+View(sapply(runif(10),r_p_s))#good luck
