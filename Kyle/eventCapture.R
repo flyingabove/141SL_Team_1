f6e3cf96f4946a778d24ac25f52ab6e6531c7824
Kyle Colton <ky.colton@gmail.com>
2015-05-13 10:49:32 -0700

    Added a description and license.

--- Kyle/eventCapture.R
+++ Kyle/eventCapture.R
@@ -1,5 +1,25 @@
 #!/usr/bin/Rscript
 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+# DESCRIPTION
+#
+# Searches for all CSV files, reads them in, identifies sections of
+# silence, and segments the data. Then does a summary analysis on each
+# individual section and outputs all the data in a single csv.
+#
+
 # Length, 5 number summary, mean
 events <- matrix(,ncol=8,nrow=0)
 colnames(events) <- c('File.Name','Min','1Q','Median','Mean','3Q','Max','Length')
