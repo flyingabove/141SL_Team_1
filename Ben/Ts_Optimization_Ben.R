74bbff696abe387ad81584820277bdec4d4dc13d
Kyle Colton <ky.colton@gmail.com>
2015-05-22 10:20:11 -0700

    Fixed the issues marked on the previous commit, I think.

--- Ben/Ts_Optimization_Ben.R
+++ Ben/Ts_Optimization_Ben.R
@@ -31,10 +31,8 @@ local_max<-function(ts_signal1_test,time_vector=0){
   CURRENT<-((ts_signal1_test[2:n] - ts_signal1_test[1:(n-1)]) <0)
   PAST<- (c(0,CURRENT[-(n-1)]) >= 0)
   max.index <- which(CURRENT & PAST)
-  max_ts_signal_time[max.index] = (((1:n) * (time_vector==0)) + time_vector)[max.index]
-  max_ts_signal_y[max.index] = (ts_signal1_test[2:n] - ts_signal1_test[1:(n-1)])[max.index]
-  max_ts_signal_time <- max_ts_signal_time[!is.na(max_ts_signal_time)]
-  max_ts_signal_y <- max_ts_signal_y[!is.na(max_ts_signal_y)]
+  max_ts_signal_time = (((1:n) * (time_vector==0)) + time_vector)[max.index]
+  max_ts_signal_y <- ts_signal1_test[max_ts_signal_time]
   return(list(max_ts_signal_time,max_ts_signal_y))
 }
 local_min<-function(ts_signal1_test,time_vector=0){
