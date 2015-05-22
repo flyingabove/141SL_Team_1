2419350fd1abe90db87a47dae7995b2212697782
Kyle Colton <ky.colton@gmail.com>
2015-05-21 21:20:25 -0700

    Reverting because of the weird merge commit. This is Ben's version and it worked (as far as I know).

--- Ben/Ts_Optimization_Ben.R
+++ Ben/Ts_Optimization_Ben.R
@@ -24,39 +24,42 @@ str(ts_signal1)
 
 #3rd Attempt
 
-local_max2<-function(ts_signal1_test,time_vector=0){
-  max_ts_signal_time <- c()
-  max_ts_signal_y<- c()
-  n <- length(ts_signal1_test)
-  CURRENT<-((ts_signal1_test[2:n] - ts_signal1_test[1:(n-1)]) <0)
-  PAST<-c((0,CURRENT[-(n-1)]) >= 0)
-  max.index <- which(CURRENT * PAST)
-  if(!time_vector) max_ts_signal_time[max.index] = max.index
-  if(time_vector) max_ts_signal_time[max.index] = time_vector[max.index]
-  max_ts_signal_y[max.index] = (ts_signal1_test[2:n] - ts_signal1_test[1:(n-1)])[max.index]
-  max_ts_signal_time <- max_ts_signal_time[!is.na(max_ts_signal_time)]
-  max_ts_signal_y <- max_ts_signal_y[!is.na(max_ts_signal_y)]
-  return(list(max_ts_signal_time,max_ts_signal_y))
-}
-
-local_min2 <- function(ts_signal1_test,time_vector=0){
-  min_ts_signal_time <- c()
-  min_ts_signal_y <- c()
+local_max<-function(ts_signal1_test,time_vector=0){
+  max_ts_signal_time<-NULL
+  max_ts_signal_y<-NULL
   CURRENT<-0
   PAST<-0
   for(i in 1:(length(ts_signal1_test)-1)){
     t1<-ts_signal1_test[i]
     t2<-ts_signal1_test[i+1]
     CURRENT<-t2-t1
-    if((CURRENT>0)&(PAST<=0)){
-      if(time_vector==0) min_ts_signal_time[i] <- i
-      if(time_vector!=0) min_ts_signal_time[i] <- time_vector[i]
-      min_ts_signal_y[i] <- t1
+    print(i)
+    if((CURRENT<0)&(PAST>=0)){
+      if(time_vector==0) max_ts_signal_time=c(max_ts_signal_time,i)
+      if(time_vector!=0) max_ts_signal_time=c(max_ts_signal_time,time_vector[i])
+      max_ts_signal_y=c(max_ts_signal_y,t1)
     }
     PAST<-CURRENT
   }
-  min_ts_signal_time <- min_ts_signal_time[!is.na(min_ts_signal_time)]
-  min_ts_signal_y <- min_ts_signal_y[!is.na(min_ts_signal_y)]
+  return(list(max_ts_signal_time,max_ts_signal_y))
+}
+local_min<-function(ts_signal1_test,time_vector=0){
+  min_ts_signal_time<-NULL
+  min_ts_signal_y<-NULL
+  CURRENT<-0
+  PAST<-0
+  for(i in 1:(length(ts_signal1_test)-1)){
+    t1<-ts_signal1_test[i]
+    t2<-ts_signal1_test[i+1]
+    CURRENT<-t2-t1
+    print(i)
+    if((CURRENT>0)&(PAST<=0)){
+      if(time_vector==0) min_ts_signal_time=c(min_ts_signal_time,i)
+      if(time_vector!=0) min_ts_signal_time=c(min_ts_signal_time,time_vector[i])
+      min_ts_signal_y=c(min_ts_signal_y,t1)
+    }
+    PAST<-CURRENT
+  }
   return(list(min_ts_signal_time,min_ts_signal_y))
 }
 
@@ -158,38 +161,6 @@ get_intervals<-function(ts_signal1){
   
   min_x<-local_min(lw_ts$y,lw_ts$x)[[1]]
   min_y<-local_min(lw_ts$y,lw_ts$x)[[2]]
-  
-  return(min_x[-1])
-  
-  
-}
-
-get_intervals2<-function(ts_signal1){
-  LOWESS_F=.15
-  MAX_POINTS=200
-  
-  ts_signal1_test<-ts_signal1
-  max_results<-local_max2(ts_signal1_test,1:length(ts_signal1_test))
-  max_ts_signal_time_i<-as.numeric(unlist(max_results[1]))
-  max_ts_signal_yi<-as.numeric(unlist(max_results[2]))
-  
-  while(length(max_ts_signal_yi)>=200){
-    ts_signal_i_test<-max_ts_signal_yi
-    ts_signal_i_time<-max_ts_signal_time_i
-    max_results_i<-local_max2(ts_signal_i_test,ts_signal_i_time)
-    max_ts_signal_time_i<-as.numeric(unlist(max_results_i[1]))
-    max_ts_signal_yi<-as.numeric(unlist(max_results_i[2]))
-  }
-  
-  plot(ts_signal1_test)
-  lines(max_ts_signal_time_i,max_ts_signal_yi,col="blue")
-  points(max_ts_signal_time_i,max_ts_signal_yi,col="red")
-  interp_signal<-approx(max_ts_signal_time_i,max_ts_signal_yi,n=1000)
-  lw_ts<-lowess(interp_signal,f=.15)
-  lines(lw_ts$x,lw_ts$y,col="yellow",lwd=8)
-  
-  min_x<-local_min2(lw_ts$y,lw_ts$x)[[1]]
-  min_y<-local_min2(lw_ts$y,lw_ts$x)[[2]]
   
   return(min_x[-1])
   
