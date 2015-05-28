8e5b3ea929e4dce3a1b30ccb9128c3ffb9aebd1f
benjaminderek <benjaminhong@s-164-67-98-242.resnet.ucla.edu>
2015-05-27 17:10:15 -0700

    changed functions

--- Ben/optimized_functions.R
+++ Ben/optimized_functions.R
@@ -1,20 +1,20 @@
-local_max<-function(ts_signal1_test,time_vector=0){
-  n <- length(ts_signal1_test)
-  CURRENT<-(ts_signal1_test[2:n] - ts_signal1_test[1:(n-1)])
-  PAST<- (c(0,CURRENT[-(n-1)]) >= 0)
-  CURRENT <- (CURRENT < 0)
-  max.index <- which(CURRENT & PAST)
-  max_ts_signal_time = (((1:n) * (time_vector==0)) + time_vector)[max.index]
-  max_ts_signal_y <- ts_signal1_test[max_ts_signal_time]
-  return(list(max_ts_signal_time,max_ts_signal_y))
-}
-local_min<-function(ts_signal1_test,time_vector=0){
-  n <- length(ts_signal1_test)
-  CURRENT<-(ts_signal1_test[2:n] - ts_signal1_test[1:(n-1)])
-  PAST<- (c(0,CURRENT[-(n-1)]) <= 0)
-  CURRENT <- (CURRENT > 0)
-  max.index <- which(CURRENT & PAST)
-  max_ts_signal_time = (((1:n) * (time_vector==0)) + time_vector)[max.index]
-  max_ts_signal_y <- ts_signal1_test[max_ts_signal_time]
-  return(list(max_ts_signal_time,max_ts_signal_y))
+local_min <- function(ts_signal1_test,time_vector=0){
+  min_ts_signal_time <- c()
+  min_ts_signal_y <- c()
+  CURRENT<-0
+  PAST<-0
+  for(i in 1:(length(ts_signal1_test)-1)){
+    t1<-ts_signal1_test[i]
+    t2<-ts_signal1_test[i+1]
+    CURRENT<-t2-t1
+    if((CURRENT>0)&(PAST<=0)){
+      if(time_vector==0) min_ts_signal_time[i] <- i
+      if(time_vector!=0) min_ts_signal_time[i] <- time_vector[i]
+      min_ts_signal_y[i] <- t1
+    }
+    PAST<-CURRENT
+  }
+  min_ts_signal_time <- min_ts_signal_time[!is.na(min_ts_signal_time)]
+  min_ts_signal_y <- min_ts_signal_y[!is.na(min_ts_signal_y)]
+  return(list(min_ts_signal_time,min_ts_signal_y))
 }
