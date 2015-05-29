694679a77cf653ac447e8c2e6c671bc3d55606bc
Kyle Colton <ky.colton@gmail.com>
2015-05-29 14:00:04 -0700

    Code to run through multiple values of K. While the prediction rate is the highest around K=30 or so, the highest power comes at k=1 or k=2, and this project is all about minimizing misclassifications of true baby_cry. All other classification is secondary.

--- Kyle/K_means.R
+++ Kyle/K_means.R
@@ -11,21 +11,30 @@ library(class)
 vars <- c(3:9)
 set.seed(5561)
 
-errors <- matrix(0,nrow=2,ncol=2)
-
-for( i in levels(long.events$File.Name))
+k.pred.rate <- vector(length=45)
+k.errors <- array(dim=c(2,2,45))
+for( j in 1:length(k.pred.rate))
 {
-    sound.knn <- knn(
-        long.events[-which(long.events$File.Name == i),vars], # train
-        long.events[which(long.events$File.Name == i),vars], # test
-        cl = long.events$Sound.Source[-which(long.events$File.Name == i)], # true classifications
-        k=1)    # k groups
-    pred.baby.cry <- FALSE
-    if('Baby_cry' %in% sound.knn){ pred.baby.cry <- TRUE }
-    actu.baby.cry <- FALSE
-    if('Baby_cry' %in% long.events$Sound.Source[which(long.events$File.Name == i)]){ actu.baby.cry <- TRUE }
-    errors[pred.baby.cry+1,actu.baby.cry+1] <- errors[pred.baby.cry+1,actu.baby.cry+1] + 1
+    errors <- matrix(0,nrow=2,ncol=2)
+
+    for( i in levels(long.events$File.Name))
+    {
+        sound.knn <- knn(
+            long.events[-which(long.events$File.Name == i),vars], # train
+            long.events[which(long.events$File.Name == i),vars], # test
+            cl = long.events$Sound.Source[-which(long.events$File.Name == i)], # true classifications
+            k=j)    # k groups
+        pred.baby.cry <- FALSE
+        if('Baby_cry' %in% sound.knn){ pred.baby.cry <- TRUE }
+        actu.baby.cry <- FALSE
+        if('Baby_cry' %in% long.events$Sound.Source[which(long.events$File.Name == i)]){ actu.baby.cry <- TRUE }
+        errors[pred.baby.cry+1,actu.baby.cry+1] <- errors[pred.baby.cry+1,actu.baby.cry+1] + 1
+    }
+    k.errors[,,j] <- errors
+    k.pred.rate[j] <- sum(diag(errors))/sum(errors)
 }
 
-sum(diag(errors))/sum(errors)
+k.pred.rate
+
+plot(k.pred.rate)
 
