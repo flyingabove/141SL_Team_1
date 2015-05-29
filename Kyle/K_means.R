8be04721145174f8846c35c80d77c2ec49a45776
Kyle Colton <ky.colton@gmail.com>
2015-05-29 13:36:10 -0700

    Update the K-means to a similar level as SVM. It has ~.369 prediction rate.

--- Kyle/K_means.R
+++ Kyle/K_means.R
@@ -1,38 +1,28 @@
-long.events <- events[which(events$Length > 5),]
+long.events <- events[which(events$Length > 18),]
+long.events$Sound.Source <- as.factor(long.events$Sound.Source)
+long.events$baby.cry <- as.factor(long.events$Sound.Source == 'Baby_cry')
 
-library(ggplot2)
-pdf('Min')
-ggplot(long.events, aes(factor(Sound.Source),Min))+geom_boxplot()
-dev.off()
-pdf('1Q.pdf')
-ggplot(long.events, aes(factor(Sound.Source),X1Q))+geom_boxplot()
-dev.off()
-pdf('Mean.pdf')
-ggplot(long.events, aes(factor(Sound.Source),Mean))+geom_boxplot()
-dev.off()
-pdf('Median.pdf')
-ggplot(long.events, aes(factor(Sound.Source),Median))+geom_boxplot()
-dev.off()
-pdf('3Q.pdf')
-ggplot(long.events, aes(factor(Sound.Source),X3Q))+geom_boxplot()
-dev.off()
-pdf('Max.pdf')
-ggplot(long.events, aes(factor(Sound.Source),Max))+geom_boxplot()
-dev.off()
-pdf('Length.pdf')
-ggplot(long.events, aes(factor(Sound.Source),Length))+geom_boxplot()
-dev.off()
+for(i in 1:nrow(long.events))
+{long.events$File.Name[i] <- unlist(strsplit(long.events$File.Name[i],fixed=F,split='[.]'))[1]}
+long.events$File.Name <- as.factor(long.events$File.Name)
 
 library(class)
 
 vars <- c(3:9)
 set.seed(5561)
 
+errors <- matrix(0,nrow=length(levels(long.events$Sound.Source)),ncol=length(levels(long.events$Sound.Source)))
 
-tt <- runif(nrow(long.events)) > .5
+for( i in levels(long.events$File.Name))
+{
+    sound.knn <- knn(
+        long.events[-which(long.events$File.Name == i),vars], # train
+        long.events[which(long.events$File.Name == i),vars], # test
+        cl = long.events$Sound.Source[-which(long.events$File.Name == i)], # true classifications
+        k=1)    # k groups
+    sound.knn.table <- table(sound.knn,long.events$Sound.Source[which(long.events$File.Name == i)])
+    errors <- errors + sound.knn.table
+}
 
-sound.knn <- knn(long.events[which(tt),vars],long.events[which(!tt),vars],
-    cl = long.events[which(tt),]$Sound.Source,k=1)
-sound.knn.table <- table(sound.knn,long.events[which(!tt),]$Sound.Source)
-sound.knn.table
-sum(diag(sound.knn.table))/sum(sound.knn.table)
+sum(diag(errors))/sum(errors)
+
